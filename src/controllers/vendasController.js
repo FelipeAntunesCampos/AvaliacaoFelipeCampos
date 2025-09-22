af5814104371f0085b15316a36c812e05661d15e
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-09-22 08:38:44 -0300

    Feat: adiciona getById

--- src/controllers/vendasController.js
+++ src/controllers/vendasController.js
@@ -7,10 +7,28 @@ const getAllVendas = (req, res) => {
         total: dadosVendas.length,
         vendas: dadosVendas
     })
-}
+};
+
+//GetById
+const getById = (req, res) => {
+    let id = parseInt(req.params.id);
+
+    const dadoVenda = dadosVendas.find(v => v.id === id);
+
+    if (dadoVenda) {
+        res.status(200).json({
+            success: true,
+            dadoVenda: dadoVenda
+        })
+    }
+
+    res.status(400).json({
+        success: false,
+        message: "Dados de vendas nao encontrados"
+    })
+};
 
 
-
-export { getAllVendas };
+export { getAllVendas, getById };
 
 
--- src/routes/vendasRoutes.js
+++ src/routes/vendasRoutes.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { getAllVendas } from "../controllers/vendasController.js";
+import { getAllVendas, getById } from "../controllers/vendasController.js";
 
 const router = express.Router();
 
-router.get("/", getAllVendas);
+router.get("/", getAllVendas); //GetAll
+router.get("/:id", getById);
+
 export default router;
